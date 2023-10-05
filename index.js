fa2ee3c70d86498294d48617bcfe69cd28607039
Adrian Di Tosto <aditosto@uoguelph.ca>
2023-10-04 22:16:36 -0400

    completed W1

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /*Step 17: obtain DOM objects */
-const getJokeBtn = document.getElementById(""); // add id
-const jokeText = document.getElementById(""); // add id
+const getJokeBtn = document.getElementById("your-button-id"); // add id
+const jokeText = document.getElementById("your-text-id"); // add id
 
-/*Step 18: Create event listener for button click */
-getJokeBtn.addEventListener("", () => { // add event listener type
+function apiCall() { 
+
     jokeText.textContent = "Fetching a joke...";
 
     /*Step 19: Create account with api-ninjas (https://api-ninjas.com/) */
@@ -13,13 +13,13 @@ getJokeBtn.addEventListener("", () => { // add event listener type
         headers: { 'X-Api-Key': 'your-api-key' } // add api key
     })
         // Checks the network response 
-        .then((response) => {
+        .then(function(response) {
 
             // If network response was not a success
             if (!response.ok) throw new Error("Network response was not ok");
             return response.json();
         })
-        .then((data) => {
+        .then(function(data) {
 
             // Update the HTML tag with the joke fetched from the response
             jokeText.textContent = data[0].joke;
@@ -27,11 +27,17 @@ getJokeBtn.addEventListener("", () => { // add event listener type
         })
 
         // Handles any errors that occur during the fetch operation
-        .catch((error) => {
+        .catch(function(error) {
             console.error("Error fetching joke:", error);
             jokeText.textContent = "An error occurred while fetching the joke.";
         });
-});
+};
+
+
+/*Step 18: Create event listener for button click */
+getJokeBtn.addEventListener("event-listner", apiCall())
+
+
 
 
 
